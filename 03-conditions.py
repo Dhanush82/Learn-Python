17e0cb4e39ef96baa88275c7923bb01c74268e7d
Dhanush <dhanushreddy82@gmail.com>
2023-08-21 07:52:23 +0530

    py

--- 03-conditions.py
+++ 03-conditions.py
@@ -1,5 +1,7 @@
-x = 5
-if x > 5:
-    print("x is greater than 5")
+x = 7
+if x > 10:
+    print("x is greater than 10")
+elif x > 5:
+    print("x is greater than 5 but not greater than 7")
 else:
     print("x is not greater than 5")
